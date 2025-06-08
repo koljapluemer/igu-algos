484f8d6d172830595970a369d4410c2c8a6762ad
koljapluemer <github@koljapluemer.com>
2025-06-08 12:37:57 +0200

    ensure library can be built again

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { ExerciseTemplate } from "./ExerciseTemplate";
+import { Lesson } from "./Lesson";
+import { ExerciseType, Generator, Exercise } from "./types";
+
+export { Lesson, ExerciseTemplate, ExerciseType, Generator, Exercise };
